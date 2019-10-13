540aed7f02d2ab8af3edd4c5c055237463e5caf4
Yorn <yornclaudel@gmail.com>
2019-10-13 23:55:21 +0200

    figuring things out v2

--- get_next_line.c
+++ get_next_line.c
@@ -10,69 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+// #include "get_next_line.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+// char *ft_substr(char const *s, unsigned int start, size_t len)
+// {
+// 	char *dest;
+// 	size_t i;
+
+// 	if (!(dest = (malloc(len + 1))))
+// 		return (0);
+// 	i = -1;
+// 	while (s[++i] && i < len)
+// 		dest[i] = s[start + i];
+// 	dest[len] = '\0';
+// 	return (dest);
+// }
+
+// char *init_buff()
+// {
+// 	char *buff;
+
+// 	if (!(buff = malloc(BUFF_SIZE * sizeof(char))))
+// 		return (0);
+// 	return (buff);
+// }
+
+// int endofline(char *str)
+// {
+// 	int i;
+
+// 	if (!str)
+// 		return (-1);
+// 	i = -1;
+// 	while (str[++i])
+// 		if (str[i] == '\n')
+// 			return (i);
+// 	return (i);
+// }
+
+// int get_next_line(int fd, char **line)
+// {
+// 	static char *left;
+// 	char *buff;
+// 	int i;
+// 	int rd;
+
+// 	buff = init_buff();
+
+// 	if (left)
+// 	{
+// 		if ((i = endofline(left)) != -1)
+// 		{
+// 			*line = ft_substr(left, 0, i);
+// 			if(i < BUFFER_SIZE)
+					
+// 		}
+// 		else
+// 		{
+
+// 		}
+// 	}
+// 	free(buff);
+// }
+
+#include <stdio.h>
+#include <unistd.h>
+int main()
 {
-	char	*dest;
-	size_t	i;
+	char test[50];
 
-	if (!(dest = (malloc(len + 1))))
-		return (0);
-	i = -1;
-	if (start < ft_strlen(s))
-		while (s[++i] && i < len)
-			dest[i] = s[start + i];
-		dest[len] = '\0';
-	return (dest);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	int l;
-
-	l = 0;
-	while (s[l])
-		l++;
-	return (l);
-}
-
-
-char *init_buff()
-{
-	char *buff;
-
-	if (!(buff = malloc(BUFF_SIZE * sizeof(char))))
-		return (0);
-	return (buff);	
-}
-
-int endofline(char *str)
-{
-	int i;
-
-	if (!str)
-		return (-1);
-	i = -1;
-	while (str[++i])
-		if (str[i] =='\n')
-	return (str[i] == 'EOF' ? i : -1);
-}
-
-int get_next_line(int fd, char **line)
-{
-	static char *left;
-	char *buff;
-	int l;
-	int rd;
-
-	l = -1;
-	buff = init_buff();
-	if ((rd = read(fd, buff, BUFF_SIZE)))
-		return (0);
-	if (rd < BUFF_SIZE)
-	{
-		
-	}
-	free(buff);
+	int fd = open("testfile", O_RDONLY);
+	read(fd,buff,50);
+	printf("%s", buff);
 }
